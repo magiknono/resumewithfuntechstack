b601f757b9a257d97783dfa6bbfbb19dfc8cd851
arnaud cormier <arno.cormier@gmail.com>
2019-10-27 22:30:37 +0100

    add styles for layout/ typo & add grid one column on wrapper

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,52 @@
 <script>
 </script>
 
+<style>
+	/* reset is in /public/global.css */
+
+	/* typo */
+	h1,h2,h3, p {
+		font-family: 'Nunito Sans', sans-serif;
+	}
+	h1 {
+		font-size:3em;
+		font-weight:bold;
+		text-transform:uppercase;
+		letter-spacing: 0.1em;
+		
+	}
+	h2 {
+		font-size:2em;
+		font-weight:lighter;
+		letter-spacing: 0.1em;
+		}
+	h3 {
+		font-size:1.5em;
+		font-weight:normal;
+	}
+
+	/* layout */
+	.wrapper {
+		 display:grid; /* use firefox dev to see grid  */
+		 grid-template-columns: 1fr;
+	}
+	.hero {
+		margin:2em;
+		padding:1.5em;
+	}
+	article {
+	}
+	section, footer, aside {
+		box-shadow: 12px 12px 2px 1px rgba(0, 0, 0, .2);
+		border: 0.15em solid rgba(0,0,0,0.5);
+		margin:2em;
+		padding:1.5em;
+	}
+	
+
+
+</style>
+
 <div class="wrapper">
 	<header class="hero">
 		<img src="https://via.placeholder.com/200" alt ="#" />
@@ -10,24 +56,6 @@
 
 	<main>
 		<article class="jobs">
-			<section class="job">
-				<img src="https://via.placeholder.com/96" alt ="#" />
-				<h3>job title</h3>
-				<p>description</p>
-				<p>2y</p>
-			</section>
-			<section class="job">
-				<img src="https://via.placeholder.com/96" alt ="#" />
-				<h3>job title</h3>
-				<p>description</p>
-				<p>2y</p>
-			</section>
-			<section class="job">
-				<img src="https://via.placeholder.com/96" alt ="#" />
-				<h3>job title</h3>
-				<p>description</p>
-				<p>2y</p>
-			</section>
 			<section class="job">
 				<img src="https://via.placeholder.com/96" alt ="#" />
 				<h3>job title</h3>
