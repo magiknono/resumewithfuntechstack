5cf6c29c51b654059d2ff9a124215ea8a1b07ec9
arnaud cormier <arno.cormier@gmail.com>
2019-10-27 21:38:02 +0100

    add html content markups with placeholder in App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,100 @@
+<div class="wrapper">
+	<header class="hero">
+		<img src="https://via.placeholder.com/200" alt ="#" />
+		<h1>magiknono - Freelance</h1>
+		<h2>Full stack dev & linux sysadmin</h2>
+	</header>
+
+	<main>
+		<article class="jobs">
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+			<section class="job">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>job title</h3>
+				<p>description</p>
+				<p>2y</p>
+			</section>
+		</article>
+
+		<article class="skills">
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<section class="skill">
+				<img src="https://via.placeholder.com/96" alt ="#" />
+				<h3>skill title</h3>
+				<p>description</p>
+				<p>High</p>
+			</section>
+			<aside class="extra-skills">
+				<img src="https://via.placeholder.com/60" alt ="#" />
+				<img src="https://via.placeholder.com/60" alt ="#" />
+			</aside>
+		</article>
+
+		<footer>
+			<nav class="socials">
+				<img src="https://via.placeholder.com/32" alt ="#" />
+				<img src="https://via.placeholder.com/32" alt ="#" />
+				<img src="https://via.placeholder.com/32" alt ="#" />
+			</nav>
+		</footer>
+
+	</main>
+</div>
